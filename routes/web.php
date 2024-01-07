5c58d06308980da372096b813434c044f69f1b76
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2024-01-07 21:50:00 +0700

    add login,register controller

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,17 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::group(['prefix'=>'/users/'],function(){
+        Route::post('/login','Auth\LoginController@Login');
+        Route::post('/register','Auth\RegisterCOntroller@register');
+    });
+});
+Route::group(['prefix'=>'/mobile'],function(){
+    Route::group(['prefix'=>'/users'],function(){
+        //
+    });
 });
