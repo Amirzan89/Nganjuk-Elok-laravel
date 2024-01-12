fb2d926593fb92c56ab3356b4d37882fe16245ad
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2024-01-12 11:11:36 +0700

    update route page tempat

--- app/Http/Controllers/Page/TempatController.php
+++ app/Http/Controllers/Page/TempatController.php
@@ -7,5 +7,64 @@ use Illuminate\Http\Request;
 
 class TempatController extends Controller
 {
-    p
+    public function showTempat(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.tempat',$dataShow);
+    }
+    public function showFormulir(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.formulir-sewa',$dataShow);
+    }
+    public function showDataTempat(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.data_tempat',$dataShow);
+    }
+    public function showPengajuan(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.pengajuan',$dataShow);
+    }
+    public function showRiwayat(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.riwayat',$dataShow);
+    }
+    public function showDetailSewa(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.detail_sewa',$dataShow);
+    }
+    public function showDetailTempat(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.home2',$dataShow);
+    }
+    public function showDetailTempatAdmin(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.detail_tempat',$dataShow);
+    }
+    public function showTambahTempat(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.tambah_tempat',$dataShow);
+    }
+    public function showEditTempat(Request $request){
+        $dataShow = [
+            ''
+        ];
+        return view('page.tempat.edit_detail_tempat',$dataShow);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\TempatController;
+
+use App\Http\Controllers\Mobile\EventController AS MobileEventController;
+use App\Http\Controllers\Mobile\PentasController AS MobilePentasController;
+use App\Http\Controllers\Mobile\TempatController AS MobileTempatController;
+use App\Http\Controllers\Mobile\SenimanController AS MobileSenimanController;
+
+use App\Http\Controllers\Page\EventController AS ShowEventController;
+use App\Http\Controllers\Page\PentasController AS ShowPentasController;
+use App\Http\Controllers\Page\SenimanController AS ShowSenimanController;
+use App\Http\Controllers\Page\TempatController AS ShowTempatController;
+
 use App\Http\Controllers\Auth\LoginController;
 Route::group(['middleware'=>'auth'],function(){
     Route::get('/login', function () {
@@ -7,9 +19,6 @@ Route::group(['middleware'=>'auth'],function(){
     })->name('login');
     Route::get('/dashboard', function () {
         return view('page.dashboard');
-    });
-    Route::get('/tempat', function () {
-        return view('page.tempat');
     });
     Route::get('/event', function () {
         return view('page.event');
@@ -54,12 +63,24 @@ Route::group(['middleware'=>'auth'],function(){
     Route::group(['prefix'=>'/pentas'],function(){
         //
     });
+    Route::group(['prefix'=>'/sewa'],function(){
+        Route::get('/formulir',[ShowTempatController::class,'showFormulir']);
+        Route::get('/detail',[ShowTempatController::class,'showDetailSewa']);
+    });
     Route::group(['prefix'=>'/tempat'],function(){
-        //
+        Route::get('/', [ShowTempatController::class,'showTempat']);
+        Route::get('/data', [ShowTempatController::class,'showDataTempat']);
+        Route::get('/pengajuan', [ShowTempatController::class,'showPengajuan']);
+        Route::get('/riwayat', [ShowTempatController::class,'showRiwayat']);
+        Route::get('/detail', [ShowTempatController::class,'showDetailTempat']);
+        Route::get('/lihat', [ShowTempatController::class,'showDetailTempat']);
+        Route::get('/tambah', [ShowTempatController::class,'showTambahTempat']);
+        Route::get('/edit', [ShowTempatController::class,'showEditTempat']);
+        Route::post('/tambah', [TempatController::class,'tambah']);
+        Route::post('/edit',[TempatController::class,'edit']);
     });
     Route::group(['prefix'=>'/users'],function(){
         Route::post('/login',[LoginController::class,'Login']);
-        // Route::post('/login','Auth\LoginController@Login');
     });
     Route::get('/auth/redirect', 'Auth\LoginController@redirectToProvider');
     Route::get('/auth/google', 'Auth\LoginController@handleProviderCallback');
